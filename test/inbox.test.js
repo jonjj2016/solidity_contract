03c765ef39a589303ffe9019e63521013b0f85b4
jonJJ2016 <jonmartirosyan1988@gmail.com>
2022-11-14 04:57:20 +0800

    added first meaningfull test

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -8,11 +8,12 @@ const web3 = new Web3(ganache.provider())
 const abi = fs.readFileSync('./contracts_Inbox_sol_Inbox.abi', 'utf-8')
 const biteCode = fs.readFileSync('./contracts_Inbox_sol_Inbox.bin', 'utf-8')
 
+let accounts
 let inbox
 beforeEach(async () => {
   // Get a list of all accounts
   // use one of those accounts to deploy the contract
-  const accounts = await web3.eth.getAccounts()
+  accounts = await web3.eth.getAccounts()
 
   inbox = await new web3.eth.Contract(JSON.parse(abi))
     .deploy({
@@ -24,5 +25,12 @@ beforeEach(async () => {
 describe('Inbox Contract test', () => {
   it('deploying contract', () => {
     assert.ok(inbox.options.address)
+  })
+  it('making transaction by calling transaction function', async () => {
+    await inbox.methods
+      .setMessage('Hello Contract')
+      .send({ from: accounts[1], gas: '1000000' })
+    const message = await inbox.methods.message().call()
+    assert.equal(message, 'Hello Contract')
   })
 })
