ee35b1353f4cac30be4ae1263b31d8ef2c1b56ad
jonJJ2016 <jonmartirosyan1988@gmail.com>
2022-11-14 03:06:16 +0800

    adding some random tests

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -3,3 +3,22 @@ const ganache = require('ganache')
 const Web3 = require('web3')
 
 const web3 = new Web3(ganache.provider())
+
+class Car {
+  park() {
+    return 'stopped'
+  }
+  drive() {
+    return 'driving'
+  }
+}
+describe('Car', () => {
+  it('parking the car', () => {
+    const car = new Car()
+    assert.equal(car.park(), 'stopped')
+  })
+  it('car is driving', () => {
+    const car = new Car()
+    assert(car.drive(), 'driving')
+  })
+})
