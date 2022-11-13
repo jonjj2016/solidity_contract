867d15c9b6a4a993b0e469e8bc42244386bf36de
jonJJ2016 <jonmartirosyan1988@gmail.com>
2022-11-14 04:37:40 +0800

    testing deployment with mocha web3 and ganche

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,27 +1,28 @@
 const assert = require('assert')
 const ganache = require('ganache')
 const Web3 = require('web3')
+const fs = require('fs')
 
 const web3 = new Web3(ganache.provider())
 
-class Car {
-  park() {
-    return 'stopped'
-  }
-  drive() {
-    return 'driving'
-  }
-}
-let account
+const abi = fs.readFileSync('./contracts_Inbox_sol_Inbox.abi', 'utf-8')
+const biteCode = fs.readFileSync('./contracts_Inbox_sol_Inbox.bin', 'utf-8')
+
+let inbox
 beforeEach(async () => {
   // Get a list of all accounts
   // use one of those accounts to deploy the contract
   const accounts = await web3.eth.getAccounts()
-  account = accounts[0]
+
+  inbox = await new web3.eth.Contract(JSON.parse(abi))
+    .deploy({
+      data: biteCode,
+    })
+    .send({ from: accounts[0], gas: '1000000' })
 })
 
-describe('Car', () => {
+describe('Inbox Contract test', () => {
   it('deploying contract', () => {
-    console.log(account)
+    assert.ok(inbox.options.address)
   })
 })
