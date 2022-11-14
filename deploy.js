eccf0a2338d6d1aba7305494c48638d94fe5bb67
jonJJ2016 <jonmartirosyan1988@gmail.com>
2022-11-15 02:13:36 +0800

    deployed contract to Goerly network

---
+++ deploy.js
@@ -0,0 +1,29 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const Web3 = require('web3')
+const fs = require('fs')
+require('dotenv').config()
+
+const biteCode = fs.readFileSync('./contracts_Inbox_sol_Inbox.bin', 'utf-8')
+const abiCode = fs.readFileSync('./contracts_Inbox_sol_Inbox.abi', 'utf-8')
+
+const provider = new HDWalletProvider(
+  process.env.MNEUMONIC,
+  process.env.RPC_URL,
+)
+const web3 = new Web3(provider)
+const main = async () => {
+  try {
+    const accounts = await web3.eth.getAccounts()
+    const contract = await new web3.eth.Contract(JSON.parse(abiCode))
+      .deploy({
+        data: biteCode,
+      })
+      .send({ from: accounts[1], gas: '1000000' })
+    console.log(contract)
+    process.exit()
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+main()
